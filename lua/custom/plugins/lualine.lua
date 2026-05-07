87deeef63e44a3ad2ac4775ccd366b73f6da306e
txriq03 <taqbro@gmail.com>
2026-05-07 11:30:33 +0100

    Change lualine to slanted

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,6 +2,11 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup {}
+    require('lualine').setup {
+      options = {
+        component_separators = '',
+        section_separators = { left = '', right = '' },
+      },
+    }
   end,
 }
