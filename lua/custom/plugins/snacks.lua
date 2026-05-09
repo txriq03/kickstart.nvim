e3585f4c13143986ee2ba07a920ecb5234b3f9b0
txirq03 <taqbro@gmail.com>
2026-05-09 02:07:43 +0100

    add snacks.nvim

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,23 @@
+vim.pack.add { 'https://github.com/folke/snacks.nvim' }
+
+local Snacks = require 'snacks'
+
+Snacks.setup {
+  bigfile = { enabled = true },
+  indent = { enabled = true },
+  input = { enabled = true },
+  lazygit = { enabled = true },
+  notifier = {
+    enabled = true,
+    timeout = 3000,
+  },
+  quickfile = { enabled = true },
+  scope = { enabled = true },
+  scroll = { enabled = true },
+  statuscolumn = { enabled = true },
+  words = { enabled = true },
+}
+
+vim.keymap.set('n', '<leader>lg', function()
+  Snacks.lazygit()
+end, { desc = 'LazyGit' })
