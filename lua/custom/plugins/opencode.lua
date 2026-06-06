4de6a611c470086f5b066c0fef2c32df35588227
txriq03 <taqbro@gmail.com>
2026-06-06 15:18:27 +0100

    Add more opencode keymaps

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -9,7 +9,7 @@ vim.g.opencode_opts = vim.g.opencode_opts or {}
 local opencode = require 'opencode'
 
 vim.keymap.set({ 'n', 'x' }, '<leader>oa', function()
-  opencode.ask('@this: ', { submit = true })
+  opencode.ask '@this: '
 end, { desc = 'OpenCode ask' })
 
 vim.keymap.set('n', '<leader>os', function()
@@ -19,6 +19,34 @@ end, { desc = 'OpenCode select' })
 vim.keymap.set('n', '<leader>op', function()
   opencode.prompt '@this '
 end, { desc = 'OpenCode prompt this' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>oe', function()
+  opencode.prompt 'Explain @this and its context'
+end, { desc = 'OpenCode explain this' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>or', function()
+  opencode.prompt 'Review @this for correctness and readability'
+end, { desc = 'OpenCode review this' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>od', function()
+  opencode.prompt 'Add comments documenting @this'
+end, { desc = 'OpenCode document this' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>oi', function()
+  opencode.prompt 'Implement @this'
+end, { desc = 'OpenCode implement this' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>ot', function()
+  opencode.prompt 'Add tests for @this'
+end, { desc = 'OpenCode test this' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>of', function()
+  opencode.prompt 'Fix @this'
+end, { desc = 'OpenCode fix this' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>oo', function()
+  opencode.prompt 'Optimize @this for performance and readability'
+end, { desc = 'OpenCode optimize this' })
 
 vim.keymap.set({ 'n', 'x' }, '<leader>ov', function()
   return opencode.operator '@this '
