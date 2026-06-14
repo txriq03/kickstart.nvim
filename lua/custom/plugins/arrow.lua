fd915fd8fd3039e5840532b9e06f1deb1865e016
txriq03 <taqbro@gmail.com>
2026-06-14 10:28:32 +0100

    Change keymap for arrow

--- lua/custom/plugins/arrow.lua
+++ lua/custom/plugins/arrow.lua
@@ -2,6 +2,6 @@ vim.pack.add { 'https://github.com/otavioschwanck/arrow.nvim' }
 
 require('arrow').setup {
   show_icons = true,
-  leader_key = ';',
+  leader_key = '<leader>a',
   buffer_leader_key = 'm',
 }
