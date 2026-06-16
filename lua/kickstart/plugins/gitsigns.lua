f8345cfdc5996fb9ac94ab344438faad50be24b3
txriq03 <taqbro@gmail.com>
2026-06-16 11:25:49 +0100

    Git blame line enabled by default

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -5,6 +5,7 @@
 vim.pack.add { 'https://github.com/lewis6991/gitsigns.nvim' }
 
 require('gitsigns').setup {
+  current_line_blame = true,
   on_attach = function(bufnr)
     local gitsigns = require 'gitsigns'
 
